f5e16ac5311612cdaf0462fe8a0408f08bbf3ecf
fatemearyaie <fatemearyaie9@gmail.com>
2025-08-20 10:45:19 +0330

    remove email from form

--- user/forms.py
+++ user/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Food
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.contrib.auth.forms import UserCreationForm
 
 class FoodForm(forms.ModelForm):
@@ -9,12 +9,12 @@ class FoodForm(forms.ModelForm):
         model = Food
         fields = ['food_name', 'description', 'price', 'day_of_week']
 
-
-
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    groups = forms.ModelMultipleChoiceField(
+        queryset=Group.objects.all(),
+        required=False,
+    )
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
-
+        fields = ("username", "password1", "password2", "groups")
